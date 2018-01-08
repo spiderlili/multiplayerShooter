104a1cb80fb23b8469c4a5d97fe30895d9cf983d
Jing Chun Tan <jtan1993@gmail.com>
2018-01-08 13:09:52 +0000

    Update PlayerHealth.cs
    net

--- Assets/Standard Assets/CrossPlatformInput/Scripts/scripts/PlayerHealth.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 //tracks player health and damage
 public class PlayerHealth : MonoBehaviour {
