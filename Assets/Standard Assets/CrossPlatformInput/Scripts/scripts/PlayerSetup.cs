acfdc938b48aa8b3ca914dd997dcbfcb7290b2b3
Jing Chun Tan <jtan1993@gmail.com>
2018-01-08 13:10:16 +0000

    Update PlayerSetup.cs
    using UnityEngine.Networking;

--- Assets/Standard Assets/CrossPlatformInput/Scripts/scripts/PlayerSetup.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/scripts/PlayerSetup.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 //ensure components exist at all time for the player
 [RequireComponent(typeof(PlayerShoot))]
